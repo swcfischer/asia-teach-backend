1376b5bd7fd89a395e64357005557b0a8252d092
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-05 13:19:31 -0500

    changed header case

--- routes/users/users.js
+++ routes/users/users.js
@@ -219,7 +219,7 @@ router.post('/login', async (req, res) => {
       { expiresIn: '365d' },
       (err, token) => {
         console.log(err);
-        res.setHeader('auth-token', token);
+        res.setHeader('Auth-Token', token);
         return res.json({
           currentUser: {
             email: user.email,
