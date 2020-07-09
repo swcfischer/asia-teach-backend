9b0fa600aadeb9444f23b36a3fe380b7d625baf3
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-09 13:56:44 -0500

    reset resumes

--- models/index.js
+++ models/index.js
@@ -29,7 +29,7 @@ Resume.belongsTo(User);
 
 User.sync({ force: false });
 Job.sync({ force: false });
-Resume.sync({ force: false });
+Resume.sync({ force: true });
 
 module.exports = {
   Sequelize,
