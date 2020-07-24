9af805bcce8fecb7eacd60847792b952c2215eb8
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-24 17:17:41 -0500

    changed webhook

--- routes/checkout/checkout.js
+++ routes/checkout/checkout.js
@@ -55,16 +55,15 @@ router.post('/payments-jobs', async (req, res) => {
 
 // Match the raw body to content type application/json
 router.post(
-  '/webhook',
+  '/webhooks',
   bodyParser.raw({ type: 'application/json' }),
   (request, response) => {
     const event = request.body;
-
-    console.log(event);
     // Handle the event
     switch (event.type) {
       case 'payment_intent.succeeded':
         const paymentIntent = event.data.object;
+        console.log('paymentIntent', paymentIntent);
         console.log('PaymentIntent was successful!');
         break;
       case 'payment_method.attached':
@@ -73,12 +72,13 @@ router.post(
         break;
       // ... handle other event types
       default:
-        // Unexpected event type
-        return response.status(400).end();
+      // Unexpected event type
+      // return response.status(400).end();
     }
 
     // Return a 200 response to acknowledge receipt of the event
-    response.json({ received: true });
+
+    return response.json({ received: true });
   }
 );
 
