33fc9e572e8ccafa84fbb7940144e58a2f729b74
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-25 18:41:13 -0500

    added subscription

--- routes/checkout/checkout.js
+++ routes/checkout/checkout.js
@@ -190,4 +190,62 @@ router.post('/check-sub', async (req, res) => {
   }
 });
 
+router.get(
+  '/subscription/:userUuid/:subscriptionId',
+  isAuthorized,
+  async (req, res) => {
+    const { userUuid, subscriptionId } = req.params;
+    const result = await stripe.subscriptions.retrieve(subscriptionId);
+    return res.json({ isActive: result.cancel_at_period_end });
+  }
+);
+
+router.post(
+  '/subscription/cancel/:userUuid/:subscriptionId',
+  isAuthorized,
+  async (req, res) => {
+    const { userUuid, subscriptionId } = req.params;
+
+    try {
+      const result = await stripe.subscriptions.update(subscriptionId, {
+        cancel_at_period_end: true,
+      });
+
+      return res.json({
+        error: false,
+        message: 'Subscription updated',
+      });
+    } catch (err) {
+      return res.json({
+        error: true,
+        message: err.message,
+      });
+    }
+  }
+);
+
+router.post(
+  '/subscription/renew/:userUuid/:subscriptionId',
+  isAuthorized,
+  async (req, res) => {
+    const { userUuid, subscriptionId } = req.params;
+
+    try {
+      const result = await stripe.subscriptions.update(subscriptionId, {
+        cancel_at_period_end: false,
+      });
+
+      return res.json({
+        error: false,
+        message: 'Subscription updated',
+      });
+    } catch (err) {
+      return res.json({
+        error: true,
+        message: err.message,
+      });
+    }
+  }
+);
+
 module.exports = router;
