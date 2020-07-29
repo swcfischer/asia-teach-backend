9fed813671b567726b0f5fca2b3975852d540c57
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-07-29 18:01:11 -0500

    drop user

--- models/index.js
+++ models/index.js
@@ -27,7 +27,7 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: false });
+User.sync({ force: true });
 Job.sync({ force: false });
 Resume.sync({ force: false });
 
