aafa85a95830f999469cabc022c27470a657d663
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-09-25 08:53:09 -0500

    dropping and recreating the resume and coupon database

--- models/index.js
+++ models/index.js
@@ -28,10 +28,10 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: true });
-Job.sync({ force: true });
-Resume.sync({ force: false });
-Coupon.sync({ force: false });
+User.sync({ force: false });
+Job.sync({ force: false });
+Resume.sync({ force: true });
+Coupon.sync({ force: true });
 
 module.exports = {
   Sequelize,
