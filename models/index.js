dd4f0edf9f4a1933a0611cdc1659e2f1a43c4241
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-09-27 10:42:44 -0500

    turning off dropping the database

--- models/index.js
+++ models/index.js
@@ -30,7 +30,7 @@ Resume.belongsTo(User);
 
 User.sync({ force: false });
 Job.sync({ force: false });
-Resume.sync({ force: true });
+Resume.sync({ force: false });
 Coupon.sync({ force: false });
 
 module.exports = {
