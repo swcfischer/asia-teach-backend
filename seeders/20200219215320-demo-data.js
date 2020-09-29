33c1405acf6fe007a9f4b8c9f8c739bfa5c35a80
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-09-29 12:43:59 -0500

    modifying seed file

--- seeders/20200219215320-demo-data.js
+++ seeders/20200219215320-demo-data.js
@@ -15,9 +15,9 @@ module.exports = {
     const users = createUsers(ids);
     const resumes = createResumes(ids);
     const coupons = createCouponCodes();
-    await queryInterface.bulkInsert('users', users, {});
-    await queryInterface.bulkInsert('resumes', resumes, {});
-    await queryInterface.bulkInsert('jobs', jobs, {});
+    // await queryInterface.bulkInsert('users', users, {});
+    // await queryInterface.bulkInsert('resumes', resumes, {});
+    // await queryInterface.bulkInsert('jobs', jobs, {});
     await queryInterface.bulkInsert('coupons', coupons, {});
     return;
   },
