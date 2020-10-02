abbfa5b905c783a0cc8367ce524b797a28dbfaba
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-10-01 21:36:21 -0500

    undropping db

--- models/index.js
+++ models/index.js
@@ -28,7 +28,7 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: true });
+User.sync({ force: false });
 Job.sync({ force: false });
 Resume.sync({ force: false });
 Coupon.sync({ force: false });
