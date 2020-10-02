58067d25f8cb4544044555ede77f7ed0c13aca7e
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-10-01 19:06:13 -0500

    dropping user db

--- util/index.js
+++ util/index.js
@@ -193,7 +193,7 @@ const countries = [
 function createCouponCodes() {
   const couponArray = [];
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 500; i++) {
     couponArray.push({
       uuid: uuidv4(),
       createdAt: new Date(),
