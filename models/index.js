365de66db13a121a788d5ec072ac722e50001af5
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-02-07 09:01:30 -0600

    dropping user and coupon databases

--- models/index.js
+++ models/index.js
@@ -28,10 +28,10 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: false });
+User.sync({ force: true });
 Job.sync({ force: false });
 Resume.sync({ force: false });
-Coupon.sync({ force: false });
+Coupon.sync({ force: true });
 
 module.exports = {
   Sequelize,
