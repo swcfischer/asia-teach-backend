43653e236feb7fc0d2ac958be9fd492344652baa
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-02-07 12:29:52 -0600

    removed newer code

--- models/index.js
+++ models/index.js
@@ -28,8 +28,6 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-// How to drop the db
-
 User.sync({ force: false });
 Job.sync({ force: false });
 Resume.sync({ force: false });
