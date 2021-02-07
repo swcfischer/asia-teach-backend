003c4e800e133b5215418080682ec64c2f4a9739
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-02-07 12:37:30 -0600

    dropping users

--- models/index.js
+++ models/index.js
@@ -28,7 +28,7 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: false });
+User.sync({ force: true });
 Job.sync({ force: false });
 Resume.sync({ force: false });
 Coupon.sync({ force: false });
