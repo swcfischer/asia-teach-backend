4ddb52349d51185fc9ff3774d0916ffb1b2f2c3d
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-02-18 20:50:02 -0600

    changed back to database

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 let sequelize;
-if (process.env.HEROKU_POSTGRESQL_BROWN_URL) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BROWN_URL, {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     port: 5432,
