146ab9fc4ed723bd85385bea502352178ce4eaf6
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-04-04 10:20:51 -0500

    not dropping db

--- models/index.js
+++ models/index.js
@@ -32,7 +32,7 @@ User.hasOne(Resume);
 Resume.belongsTo(User);
 
 User.sync({ force: false });
-Job.sync({ force: true });
+Job.sync({ force: false });
 Resume.sync({ force: false });
 Coupon.sync({ force: false });
 
