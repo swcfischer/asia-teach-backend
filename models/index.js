3b3600546e9e383101443300d2d1223050e9ec61
Steven Fischer <swcfischer@gmail.com>
2023-04-04 13:59:39 -0500

    changes

--- models/index.js
+++ models/index.js
@@ -31,10 +31,10 @@ Job.belongsTo(User);
 User.hasOne(Resume);
 Resume.belongsTo(User);
 
-User.sync({ force: false });
-Job.sync({ force: false });
-Resume.sync({ force: false });
-Coupon.sync({ force: false });
+User.sync({ force: true });
+Job.sync({ force: true });
+Resume.sync({ force: true });
+Coupon.sync({ force: true });
 
 module.exports = {
   Sequelize,
